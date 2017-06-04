b44a2287dd1e80874f92c4c246d38348da7b8c89
Tim Perry <tim.perry@bbc.co.uk>
2017-06-04 16:20:42 +0100

    Graphql avr mutation setup

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (..)
 
 import Html
-import Ui.Checkbox
-import Ui.Tabs
+import State exposing (..)
+import View exposing (..)
+import Types exposing (..)
 
 
 main : Program Never Model Msg
@@ -13,68 +14,3 @@ main =
         , view = view
         , subscriptions = subscriptions
         }
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( { checkbox = Ui.Checkbox.init ()
-      , tabs = Ui.Tabs.init ()
-      }
-    , Cmd.none
-    )
-
-
-type alias Model =
-    { checkbox : Ui.Checkbox.Model
-    , tabs : Ui.Tabs.Model
-    }
-
-
-type Msg
-    = CheckboxMsg Ui.Checkbox.Msg
-    | TabsMsg Ui.Tabs.Msg
-    | AVRSetPowerState Bool
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        CheckboxMsg msg ->
-            let
-                ( subModel, subMsg ) =
-                    Ui.Checkbox.update msg model.checkbox
-            in
-                ( { model | checkbox = subModel }, Cmd.map CheckboxMsg subMsg )
-
-        TabsMsg msg ->
-            let
-                ( subModel, subMsg ) =
-                    Ui.Tabs.update msg model.tabs
-            in
-                ( { model | tabs = subModel }, Cmd.map TabsMsg subMsg )
-
-        AVRSetPowerState state ->
-            ( model, Cmd.none )
-
-
-view : Model -> Html.Html Msg
-view model =
-    let
-        avrTabContent =
-            Html.map CheckboxMsg (Ui.Checkbox.viewToggle model.checkbox)
-    in
-        Html.div []
-            [ (Ui.Tabs.view
-                { contents =
-                    [ ( "AVR", avrTabContent )
-                    ]
-                , address = TabsMsg
-                }
-                model.tabs
-              )
-            ]
-
-
-subscriptions : Model -> Sub Msg
-subscriptions model =
-    Ui.Checkbox.onChange AVRSetPowerState model.checkbox
